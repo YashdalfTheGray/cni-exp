e71d95b457a0c31cdad9eadf1a1758b5ff90197e
Yash Kulshrestha <kulshres@amazon.com>
2023-05-01 22:53:35 +0000

    added pulling the cni plugins that we pull in agent

--- initial-setup.sh
+++ initial-setup.sh
@@ -10,3 +10,9 @@ cd ..
 rm -rf plugins
 mkdir plugins
 cp amazon-ecs-agent/out/cni-plugins/* plugins
+rm -rf cni-amd64
+mkdir cni-amd64
+wget https://github.com/containernetworking/plugins/releases/download/v0.9.1/cni-plugins-linux-amd64-v0.9.1.tgz
+tar -xvzf cni-plugins-linux-amd64-v0.9.1.tgz -C cni-amd64
+cp cni-amd64/host-local plugins/host-local
+rm cni-plugins-linux-amd64-v0.9.1.tgz
